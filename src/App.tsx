4d6ca761e9773909d5bc000b72990369d81da835
shivvang <shivanghariakar@gmail.com>
2025-08-03 13:07:39 +0530

    inital project setup vite + react + typescript +  tailwind + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  
+  return (
+    <div className="text-center text-black font-semibold">
+      Severless Skill Swap Application  
+    </div>
+  )
+}
+
+export default App
